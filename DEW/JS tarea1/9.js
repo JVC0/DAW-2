495dee1269dbea26346fd9b428bf919ad98f97ab
JVC0 <josephvanegas48@gmail.com>
2024-09-22 16:56:32 +0100

    ..

--- DEW/JS tarea1/9.js
+++ DEW/JS tarea1/9.js
@@ -1,8 +1,8 @@
-function getJulianaDate(dateArg) {
+function JulianaDate(dateArg) {
     const JULIANA = 2440587.5;
     const date = new Date(dateArg);
     const unixDate = date.getTime() / 86400000; 
     return unixDate + JULIANA;
 }
 
-console.log(getJulianaDate("2018-09-20"));
+console.log(JulianaDate("2018-09-20"));
