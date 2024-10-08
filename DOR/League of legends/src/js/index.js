eb86a7f2dc04843e709158c89526e2d822796325
JVC0 <josephvanegas48@gmail.com>
2024-10-08 16:23:16 +0100

    ..

--- DOR/League of legends/src/js/index.js
+++ DOR/League of legends/src/js/index.js
@@ -18,15 +18,19 @@ async function startChampions (){
 
 
 
-const showchampions = async () => {
-    const championss = document.getElementById("championss");
-    for (let champion of champions) {
-        championss.innerHTML += `<div class="card">
-                                    <img class="Front_img" src="https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion.id}_0.jpg">
-                                    <div class= "blurb">
-                                        ${champion.blurb}
-                                    </div>
-                                    <h2>${champion.name}</h2>
-                                </div>`
-    }
-}
+    const showchampions = async () => {
+        const championss = document.getElementById("championss");
+        for (let champion of champions) {
+          championss.innerHTML += `<div class="card">
+                                      <div class="flip-card-inner">
+                                        <div class="flip-card-front">
+                                          <img class="Front_img" src="https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion.id}_0.jpg">
+                                          <h2>${champion.name}</h2>
+                                        </div>
+                                        <div class="flip-card-back">
+                                          <h1>${champion.blurb}</h1>
+                                        </div>
+                                      </div>
+                                    </div>`
+        }
+      }
