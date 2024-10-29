2662261517ed6010b7fffa93df40bc1db5eb0da9
JVC0 <josephvanegas48@gmail.com>
2024-10-29 14:36:10 +0000

    ..

---
+++ DEW/proyecto/src/script.ts
@@ -0,0 +1,14 @@
+
+
+        // Ejercicio D1S1.1.1
+        let mensaje:string  = 'hola mundo'
+        $('#output1').text(mensaje);
+        // Ejercicio D1S1.1.2
+
+        // Ejercicio D1S2.1.1
+
+        // Ejercicio D1S2.1.2
+
+        // Ejercicio D1S2.2.1
+
+        // Ejercicio D1S2.2.2
